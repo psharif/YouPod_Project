7251bfa72978e54ca476a89424a0b8f06ba09c23
unknown <jjhernandez94@gmail.com>
2019-04-22 21:54:19 -0700

    UPDATE
    Converted API calls from unirest to axios

--- Server.js
+++ Server.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const youTubeService = require('./services/YouTubeService');
-const podcastcall = require('./apis/podcast');
+const podcastCall = require('./services/PodcastService');
 const app = express();
 const bodyParser = require("body-parser");
-const port = 8080;
 const keys = require("./config/keys");
 
 app.use(bodyParser.json());
-// app.get('/', (req, res) => res.send('Hello World!'));
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+let ytService = new youTubeService();
+ytService.getResponse();
 
-// let ytService = new youTubeService();
-
-// ytService.getResponse();
-
-podcastcall();
+let podService = new podcastCall();
+podService.getResponse();
 
 const PORT = process.env.PORT || 5000;
 
--- apis/podcast.js
+++ apis/podcast.js
@@ -1,30 +1,15 @@
-const unirest = require('unirest');
 const keys = require('../config/keys');
-const KEY = '64126793557f4228a41630d59c6b7f47';
-// const axios = require('axios');
-console.log(keys.podcastKey);
+const axios = require('axios');
 
-const podcastCall = () => {
-    unirest.get('https://listen-api.listennotes.com/api/v2/search?q=49ers&type=podcast&offset=0&len_min=0&len_max=5&only_in=title%2Cdescription&language=English&ocid=&ncid=&safe_mode=0')
-    .header('X-ListenAPI-Key', keys.podcastKey)
-    .end(function (response) {
-        // console.log(response.body.results)
-        for( let i = 0; i < response.body.results.length; i++ ) {
-            console.log(response.body.results[i].rss);
-            console.log(response.body.results[i].image);
-            console.log(response.body.results[i].title_original);
-            console.log(response.body.results[i].description_original);
-            console.log('                                              ');
-        }
-    });
-}
-
-module.exports = podcastCall;
-
-// module.exports = axios.create({
-//   baseURL: 'https://listen-api.listennotes.com/api/v2',
-//   params:{
-//     key: KEY,
-//     type: 'podcast'
-//   }
-// });
+module.exports = axios.create({
+  baseURL: 'https://listen-api.listennotes.com/api/v2',
+  headers: {'X-ListenAPI-Key': keys.podcastKey},
+  params:{ 
+    q: '49ers',
+    type: 'podcast',
+    len_min: 0,
+    len_max: 5,
+    language: 'English',
+    safe_mode: 0
+  }
+});

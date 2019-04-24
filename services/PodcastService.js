e56002c748c105001735c89fd0cb0b080aafbe02
psharif <psharif2788@gmail.com>
2019-04-23 23:04:30 -0700

    Added new files from master

--- services/PodcastService.js
+++ services/PodcastService.js
@@ -2,19 +2,18 @@ const podcall = require('../apis/podcast');
 
 class PodcastService {
     constructor(){}
- 
+
     async getResponse () {
-        try {
-            const response = await podcall.get('/search')
-            console.log(response.data.results);
-            for( let i = 0; i < response.data.results.length; i++ ) {
-                            console.log(response.data.results[i].title_original);
-                            console.log(response.data.results[i].description_original);
-                            console.log('                                              ');
-                        }
-        } catch (error) {
-            console.error(error);
-          }
+      try {
+        const response = await podcall.get('/search')
+        for( let i = 0; i < response.data.results.length; i++ ) {
+          console.log(response.data.results[i].title_original);
+          console.log(response.data.results[i].description_original);
+          console.log('                                              ');
+        }
+      } catch (error) {
+        console.error(error);
+      }
     }
  }
 

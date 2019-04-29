d6839c6cd1af82b9a64601ffc78ff597eb4dfdac
Dima Kurbatov <dskurbatov13@gmail.com>
2019-04-28 17:31:38 -0700

    removed express instance middleware and routes to separate file

--- Server/Server.js
+++ Server/Server.js
@@ -1,26 +1,6 @@
-const express = require('express');
-const youTubeService = require('./services/YouTubeService');
-const podcastCall = require('./services/PodcastService');
-const app = express();
-const bodyParser = require("body-parser");
-const keys = require("./config/keys");
-const cors = require('cors');
+const app = require('./src/app')
 
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get('/videos', (req, res) => {
-
-  const ytService = new youTubeService();
-
-  ytService.getVideos(req.query.term)
-    .then( videos => res.json(videos) );
-
-});
-
-let podService = new podcastCall();
-podService.getResponse();
-
+//define port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function() {
